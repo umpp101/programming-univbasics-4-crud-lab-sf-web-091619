ace5bdd70e38d61fc111869bf1d3ef94dc488c25
Mustafa Muse <umpp101@icloud.com>
2019-09-05 21:27:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,19 +19,19 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  [].shift(element)
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[]
 end
 
 def retrieve_first_element_from_array(array)
-  [][1]
+  array[1]
 end
 
 def retrieve_last_element_from_array(array)
-  [][-1]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
